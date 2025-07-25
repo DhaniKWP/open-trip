4323619b5ebe399b3fcad7edc58cdb096973c623
DhaniKWP <dhanikusuma256@gmail.com>
2025-07-25 15:30:32 +0700

    endpoint trip

---
+++ backend/controllers/trip.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"backend/database"
+	"backend/models"
+)
+
+func GetTrips(c *gin.Context) {
+	var trips []models.Trip
+	database.DB.Find(&trips)
+	c.JSON(http.StatusOK, trips)
+}
+
+func CreateTrip(c *gin.Context) {
+	var input models.Trip
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	database.DB.Create(&input)
+	c.JSON(http.StatusOK, input)
+}
