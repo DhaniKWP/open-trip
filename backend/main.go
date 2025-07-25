7f6a8e8c0e693bcb24eae56de3829886e9784aba
DhaniKWP <dhanikusuma256@gmail.com>
2025-07-25 15:32:23 +0700

    Entry Point

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"backend/database"
+	"backend/models"
 	"backend/routes"
 )
 
 func main() {
 	database.ConnectDB()
+	database.DB.AutoMigrate(&models.Trip{})
+
 	r := routes.SetupRouter()
 	r.Run(":8080")
 }
-
