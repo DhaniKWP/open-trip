5a6a09a5bf908f0d99828b01411b40fdf1a21ac0
DhaniKWP <dhanikusuma256@gmail.com>
2025-07-25 15:31:48 +0700

    Routing API

---
+++ backend/routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"backend/controllers"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	api := r.Group("/api")
+	{
+		api.GET("/trips", controllers.GetTrips)
+		api.POST("/trips", controllers.CreateTrip)
+	}
+
+	return r
+}
